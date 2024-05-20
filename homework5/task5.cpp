48fbc98c0e60053424d92efb218d60515318f48a
radoslav-slavov <r.slavov22@gmail.com>
2024-05-20 12:32:54 +0300

    hw5 task5

---
+++ homework5/task5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+    int n, num, digit, rev = 0;
+
+    std::cout << "Enter a positive number: ";
+    std::cin >> num;
+
+    n = num;
+
+    do
+    {
+        digit = num % 10;
+        rev = (rev * 10) + digit;
+        num = num / 10;
+    } while (num != 0);
+
+    if (n == rev)
+        std::cout << "The number is a palindrome." << std::endl;
+    else
+        std::cout << "The number is not a palindrome." << std::endl;
+
+    return 0;
+}
